1437a67a56f54bb642acbd6ba68c41ed6af96009
sumin <jss05157@naver.com>
2024-11-17 22:03:02 +0900

    no message

---
+++ src/test/java/com/swengineer/sportsmatch/SportsmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swengineer.sportsmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsmatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
